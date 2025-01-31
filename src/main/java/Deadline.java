215ff905815b5d09c7ab09c126c2ca8175af48a6
feconi1024 <87801895+feconi1024@users.noreply.github.com>
2025-01-31 15:52:25 +0800

    Add Deadline class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,16 @@
+public class Deadline extends Task{
+    private String endTime;
+
+    public Deadline(String taskName, String endTime){
+        super(taskName);
+        this.endTime = endTime;
+    }
+
+    public String getEndTime(){
+        return endTime;
+    }
+
+    public String toString(){
+        return "[D]" + super.toString() + " (by: " + getEndTime() + ")";
+    }
+}

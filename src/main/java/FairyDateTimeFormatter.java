1975001b58aa2a71e4910ebe96f925db08e931d6
feconi1024 <hzactix@github.com>
2025-02-22 23:28:23 +0800

    Add search task by date

--- src/main/java/FairyDateTimeFormatter.java
+++ src/main/java/FairyDateTimeFormatter.java
@@ -1,16 +1,22 @@
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeParseException;
 
 public class FairyDateTimeFormatter {
     private static final String FORMAT_TIME_INPUT = "yyyyMMdd HHmm";
     private static final String FORMAT_TIME_OUTPUT = "dd LLL yyyy HH:mm";
+    private static final String FORMAT_DATE_INPUT = "yyyyMMdd";
 
     private static final DateTimeFormatter FORMATTER_TIME_INPUT = DateTimeFormatter.ofPattern(FORMAT_TIME_INPUT);
     private static final DateTimeFormatter FORMATTER_TIME_OUTPUT = DateTimeFormatter.ofPattern(FORMAT_TIME_OUTPUT);
+    private static final DateTimeFormatter FORMATTER_DATE_INPUT = DateTimeFormatter.ofPattern(FORMAT_DATE_INPUT);
 
     public static LocalDateTime parseDateTime(String date) {
         return LocalDateTime.parse(date, FORMATTER_TIME_INPUT);
+    }
+
+    public static LocalDate parseDate(String date) {
+        return LocalDate.parse(date, FORMATTER_DATE_INPUT);
     }
 
     public static String formatDateTimePrint(LocalDateTime dateTime) {

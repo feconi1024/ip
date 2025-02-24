6f430e38506da41bc9d483bb471dd5a2c531dfe1
feconi1024 <87801895+feconi1024@users.noreply.github.com>
2025-02-24 16:17:34 +0800

    Substitute with static strings in add commands

--- src/main/java/TodoCommand.java
+++ src/main/java/TodoCommand.java
@@ -2,9 +2,6 @@ public class TodoCommand extends Command{
 
     public static final String COMMAND_WORD = "todo";
 
-    public static final String MESSAGE_SUCCESS = "Yes, Master. I've added this task to your list:\n%s" +
-            "\nThere are %d tasks in your list now.";
-
     public static final int TASK_INDENT = 2;
 
     private final String taskName;
@@ -17,6 +14,7 @@ public class TodoCommand extends Command{
     @Override
     public void execute(TaskList taskList, Ui ui, Storage storage) {
         Task task = taskList.addToDo(taskName);
-        ui.printStandardFormat(String.format(MESSAGE_SUCCESS, task.toString().indent(TASK_INDENT), taskList.size()));
+        ui.printStandardFormat(String.format(Messages.MESSAGE_ADD_TASK_SUCCESS,
+                task.toString().indent(TASK_INDENT), taskList.size()));
     }
 }

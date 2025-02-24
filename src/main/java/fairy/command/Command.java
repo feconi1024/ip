9cafab18e7ab7befb7bc4f7f630be8adfaaea54f
feconi1024 <hzactix@github.com>
2025-02-25 02:23:17 +0800

    Complete JavaDoc for command package

--- src/main/java/fairy/command/Command.java
+++ src/main/java/fairy/command/Command.java
@@ -4,12 +4,27 @@ import fairy.storage.Storage;
 import fairy.task.TaskList;
 import fairy.ui.Ui;
 
+/**
+ * Represents an executable command.
+ */
 public abstract class Command {
 
     public Command() {};
 
+    /**
+     * Executes the command and shows the result on UI.
+     *
+     * @param tasks List of tasks.
+     * @param ui User interface to interact with.
+     * @param storage File loading and saving interfaces.
+     */
     public abstract void execute(TaskList tasks, Ui ui, Storage storage);
 
+    /**
+     * Returns indication of exit command.
+     *
+     * @return Indication of the command. True means exit command.
+     */
     public boolean isExit() {
         return false;
     }
--- src/main/java/fairy/command/TodoCommand.java
+++ src/main/java/fairy/command/TodoCommand.java
@@ -7,10 +7,14 @@ import fairy.task.TaskList;
 import fairy.common.Messages;
 import fairy.ui.Ui;
 
+/**
+ * Represents a command of adding a todo to the list of tasks.
+ */
 public class TodoCommand extends Command {
 
     public static final String COMMAND_WORD = "todo";
 
+    /* Indentation of task information when being shown. */
     public static final int TASK_INDENT = 2;
 
     private final String taskName;

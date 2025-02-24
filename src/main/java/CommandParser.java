a1494db83022418ed4932617d33411cdb3008ac8
feconi1024 <87801895+feconi1024@users.noreply.github.com>
2025-02-24 14:16:23 +0800

    Modify command parser to align with design

--- src/main/java/CommandParser.java
+++ src/main/java/CommandParser.java
@@ -3,16 +3,15 @@ import java.util.List;
 
 public class CommandParser {
 
-    public static List<String> parseCommand(String input) {
+    public static Command parseCommand(String input) throws InvalidCommandException {
         List<String> result = new ArrayList<>();
         if (input == null || input.isEmpty()) {
-            return result;
+            throw new InvalidCommandException();
         }
 
         // Split into command and the rest of the string
         String[] cmdSplit = input.split(" ", 2);
         String command = cmdSplit[0];
-        result.add(command);
 
         if (cmdSplit.length > 1) {
             String rest = cmdSplit[1];
@@ -26,6 +25,35 @@ public class CommandParser {
             }
         }
 
-        return result;
+        // Return respective commands
+        switch (command) {
+
+            case TodoCommand.COMMAND_WORD:
+                return new TodoCommand(result.get(0));
+
+            case DeadlineCommand.COMMAND_WORD:
+                return new DeadlineCommand(result.get(0), result.get(1));
+
+            case EventCommand.COMMAND_WORD:
+                return new EventCommand(result.get(0), result.get(1), result.get(2));
+
+            case DeleteCommand.COMMAND_WORD:
+                return new DeleteCommand(Integer.parseInt(result.get(0)));
+
+            case ListCommand.COMMAND_WORD:
+                return new ListCommand();
+
+            case MarkCommand.COMMAND_WORD:
+                return new MarkCommand(Integer.parseInt(result.get(0)));
+
+            case UnmarkCommand.COMMAND_WORD:
+                return new UnmarkCommand(Integer.parseInt(result.get(0)));
+
+            case SearchByDateCommand.COMMAND_WORD:
+                return new SearchByDateCommand(result.get(0));
+
+            default:
+                throw new InvalidCommandException();
+        }
     }
 }
--- src/main/java/FairyTaskListOutputFormatter.java
+++ src/main/java/FairyTaskListOutputFormatter.java
@@ -2,11 +2,9 @@ import java.util.Iterator;
 
 public class FairyTaskListOutputFormatter {
 
-    public static final String MESSAGE_NO_TASKS_FOUND = "No tasks found";
-
     public static String formatTaskList(Iterator<Task> tasks) throws EmptyListException {
         if (!tasks.hasNext()) {
-            throw new EmptyListException(MESSAGE_NO_TASKS_FOUND);
+            throw new EmptyListException();
         }
         StringBuilder sb = new StringBuilder();
         for (int i = 0; tasks.hasNext(); i++) {

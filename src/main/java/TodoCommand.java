6178eca5463dcaa184cd0428e0b4f8fb5b3104ce
feconi1024 <hzactix@github.com>
2025-02-24 10:26:05 +0800

    Separate TodoCommand as new class from Fairy

---
+++ src/main/java/TodoCommand.java
@@ -0,0 +1,22 @@
+public class TodoCommand extends Command{
+
+    public static final String COMMAND_WORD = "todo";
+
+    public static final String MESSAGE_SUCCESS = "Yes, Master. I've added this task to your list:\n%s" +
+            "\nThere are %d tasks in your list now.";
+
+    public static final int TASK_INDENT = 2;
+
+    private String taskName;
+
+    public TodoCommand(String taskName) {
+        this.taskName = taskName;
+    }
+
+    @Override
+    public void execute(TaskList taskList, Ui ui, Storage storage) {
+        // Substitute with returned Task later
+        taskList.addToDo(taskName);
+        Ui.printStandardFormat(String.format(MESSAGE_SUCCESS, taskName, taskList.size()));
+    }
+}

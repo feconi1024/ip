c3f88b5b2d80dc45f8eecf1e9a8ca0445ab54af0
feconi1024 <hzactix@github.com>
2025-02-24 14:16:23 +0800

    Modify command parser to align with design

--- src/main/java/EmptyListException.java
+++ src/main/java/EmptyListException.java
@@ -1,4 +1,9 @@
 public class EmptyListException extends Exception{
+
+    public EmptyListException(){
+        super();
+    }
+
     public EmptyListException(String message){
         super(message);
     }

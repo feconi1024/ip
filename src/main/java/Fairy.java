f4d6aefc864fa8ad1b5a29b596b93e9a161948ec
feconi1024 <hzactix@github.com>
2025-02-24 14:48:58 +0800

    Modify main class to align with design

--- src/main/java/Fairy.java
+++ src/main/java/Fairy.java
@@ -3,100 +3,44 @@ import java.util.List;
 
 public class Fairy {
     public static final String NAME = "Fairy";
-    public static final TaskList TASKS = new TaskList();
     private static final String FILE = "./data/fairytasks.txt";
     private static final String DIR = "./data/";
 
-    private static int session(Ui ui) {
-        while (true) {
-            List<String> command = CommandParser.parseCommand(ui.prompt());
-            switch (command.get(0)) {
-                case "bye":
-                    return 0;
-                case "mark":
-                    try {
-                        TASKS.markTask(Integer.parseInt(command.get(1)));
-                    } catch (IndexOutOfBoundsException e) {
-                        if (command.size() < 2) {
-                            ui.argumentExceptionMessage();
-                        } else {
-                            ui.indexOutOfBoundsMessage(e);
-                        }
-                    }
-                    break;
-                case "unmark":
-                    try {
-                        TASKS.unmarkTask(Integer.parseInt(command.get(1)));
-                    } catch (IndexOutOfBoundsException e) {
-                        if (command.size() < 2) {
-                            ui.argumentExceptionMessage();
-                        } else {
-                            ui.indexOutOfBoundsMessage(e);
-                        }
-                    }
-                    break;
-                case "list":
-                    ui.printTaskList(TASKS.iterator());
-                    break;
-                case "todo":
-                    try {
-                        TASKS.addToDo(command.get(1));
-                    } catch (IndexOutOfBoundsException e) {
-                        ui.argumentExceptionMessage();
-                    }
-                    break;
-                case "deadline":
-                    try {
-                        TASKS.addDeadline(command.get(1), command.get(2));
-                    } catch (IndexOutOfBoundsException e) {
-                        ui.argumentExceptionMessage();
-                    } catch (DateTimeException e) {
-                        ui.dateTimeExceptionMessage(e);
-                    }
-                    break;
-                case "event":
-                    try {
-                        TASKS.addEvent(command.get(1), command.get(2), command.get(3));
-                    } catch (IndexOutOfBoundsException e) {
-                        ui.argumentExceptionMessage();
-                    } catch (DateTimeException e) {
-                        ui.dateTimeExceptionMessage(e);
-                    }
-                    break;
-                case "delete":
-                    try {
-                        TASKS.deleteTask(Integer.parseInt(command.get(1)));
-                    } catch (IndexOutOfBoundsException e) {
-                        if (command.size() < 2) {
-                            ui.argumentExceptionMessage();
-                        } else {
-                            ui.indexOutOfBoundsMessage(e);
-                        }
-                    }
-                    break;
-                case "searchByDate":
-                    try {
-                        TASKS.searchTaskByDate(command.get(1));
-                    } catch (IndexOutOfBoundsException e) {
-                        ui.argumentExceptionMessage();
-                    } catch (DateTimeException e) {
-                        ui.dateTimeExceptionMessage(e);
-                    }
-                    break;
-                default:
-                    // Wrong Command
-                    ui.commandNotFoundMessage(command.get(0));
+    private Storage storage;
+    private TaskList tasks;
+    private Ui ui;
+
+    public Fairy(String name, String filePath, String fileDir) {
+        ui = new Ui(NAME);
+        storage = new Storage(fileDir, filePath);
+        tasks = new TaskList();
+        storage.readFile(tasks, ui);
+    }
+
+    public void run() {
+        ui.greetMessage();
+        boolean isExit = false;
+        while (!isExit) {
+            try {
+                String fullCommand = ui.prompt();
+                Command c = CommandParser.parseCommand(fullCommand);
+                c.execute(tasks, ui, storage);
+                isExit = c.isExit();
+            } catch (IndexOutOfBoundsException e) {
+                ui.argumentExceptionMessage();
+            } catch (NumberFormatException e) {
+                ui.numberParseExceptionMessage();
+            } catch (InvalidCommandException e) {
+                ui.commandNotFoundMessage(e.getMessage());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
         }
+        storage.saveFile(tasks, ui);
+        ui.exitMessage();
     }
 
     public static void main(String[] args) {
-        Ui ui = new Ui();
-        ui.greetMessage();
-        Storage storage = new Storage(DIR, FILE);
-        storage.readFile(TASKS, ui);
-        session(ui);
-        storage.saveFile(TASKS, ui);
-        ui.exitMessage();
+        new Fairy(NAME, FILE, DIR).run();
     }
 }

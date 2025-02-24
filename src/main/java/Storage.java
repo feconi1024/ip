bce0b0acc3b302c2793578590ef81abcc52e12de
feconi1024 <hzactix@github.com>
2025-02-24 13:42:40 +0800

    Modify Storage to align with design

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,57 +1,68 @@
 import java.io.*;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 public class Storage {
+
+    private static final String MESSAGE_RECORD_ADDED = "%d of %d lines added to the list of tasks.";
+    private static final String MESSAGE_FAILURES_EXIST =
+            " \nFailures may because of incorrect format or corrupted file.";
+    private static final String MESSAGE_FILE_NOT_FOUND = "No record found. List starts empty.";
+    private static final String MESSAGE_IO_EXCEPTION = "I/O exception: ";
+
+    private static final String MESSAGE_RECORD_SAVED = "Tasks saved.";
+    private static final String MESSAGE_ERROR_SAVING = "Error saving file: ";
 
     private final String directory;
     private final String fileName;
 
     public Storage(String dir, String filename) {
-        directory = dir;
-        fileName = filename;
+        this.directory = dir;
+        this.fileName = filename;
     }
 
-    public void readFile() {
+    public ArrayList<Task> readFile(TaskList taskList, Ui ui) {
         try {
             BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            ArrayList<Task> tasks = new ArrayList<>();
             String line;
             int effectiveLines = 0;
             int totalLines = 0;
             while ((line = reader.readLine()) != null) {
-                effectiveLines += Fairy.TASKS.addTaskFromRecord(line);
+                effectiveLines += taskList.addTaskFromRecord(line);
                 totalLines += 1;
             }
             reader.close();
             if (effectiveLines != totalLines) {
-                Ui.printStandardFormat(String.format("%d of %d lines added to the list of tasks. \n" +
-                        "Failures may because of incorrect format or corrupted file.", effectiveLines, totalLines));
+                ui.printStandardFormat(String.format(MESSAGE_RECORD_ADDED + MESSAGE_FAILURES_EXIST,
+                        effectiveLines, totalLines));
             } else {
-                Ui.printStandardFormat(String.format("%d of %d lines added to the list of tasks.",
+                ui.printStandardFormat(String.format(MESSAGE_RECORD_ADDED,
                         totalLines, effectiveLines));
             }
 
         } catch (FileNotFoundException e) {
-            Ui.printStandardFormat("No record found. List starts empty.");
+            ui.printStandardFormat(MESSAGE_FILE_NOT_FOUND);
         } catch (IOException e) {
-            Ui.printStandardFormat("I/O exception: " + e.getMessage());
+            ui.printStandardFormat(MESSAGE_IO_EXCEPTION + e.getMessage());
         }
     }
 
-    public void saveFile() {
+    public void saveFile(TaskList taskList, Ui ui) {
         File dir = new File(directory);
         if (!dir.exists()) {
             dir.mkdirs();
         }
         try {
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-            for (Iterator<Task> it = Fairy.TASKS.iterator(); it.hasNext(); ) {
+            for (Iterator<Task> it = taskList.iterator(); it.hasNext(); ) {
                 Task task = it.next();
                 writer.write(task.toFileString() + "\n");
             }
             writer.close();
-            Ui.printStandardFormat("Tasks saved.");
+            ui.printStandardFormat(MESSAGE_RECORD_SAVED);
         } catch (IOException e) {
-            Ui.printStandardFormat("Error saving file: " + e.getMessage());
+            ui.printStandardFormat(MESSAGE_ERROR_SAVING + e.getMessage());
         }
     }
 }

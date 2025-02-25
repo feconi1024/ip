0195ab5ae16f58cf021409d1829e21c05f40ff55
feconi1024 <87801895+feconi1024@users.noreply.github.com>
2025-02-26 01:51:50 +0800

    Complete JavaDoc for task package

--- src/main/java/fairy/task/Task.java
+++ src/main/java/fairy/task/Task.java
@@ -1,9 +1,17 @@
 package fairy.task;
 
+/**
+ * Represents a task.
+ */
 public class Task {
     private final String taskName;
     private boolean done;
 
+    /**
+     * Constructs the task. Task is set to uncompleted when initialized.
+     *
+     * @param taskName Description of the task.
+     */
     public Task(String taskName) {
         this.taskName = taskName;
         done = false;
@@ -13,18 +21,34 @@ public class Task {
         return taskName;
     }
 
+    /**
+     * Sets the task as completed.
+     */
     public void setDo() {
         done = true;
     }
 
+    /**
+     * Sets the task as uncompleted.
+     */
     public void setUndo() {
         done = false;
     }
 
+    /**
+     * Returns the completion status of the task.
+     *
+     * @return Completion status of the task. {@code True} if the task is completed.
+     */
     public boolean isDone() {
         return done;
     }
 
+    /**
+     * Returns the string representation of the task for saving in file.
+     *
+     * @return String representation of the task for saving in file.
+     */
     public String toFileString() {
         if (done) {
             return "T | " + taskName;
@@ -33,6 +57,11 @@ public class Task {
         }
     }
 
+    /**
+     * Returns the string representation of the task for showing on user interface.
+     *
+     * @return String representation of the task for showing on UI.
+     */
     public String toString() {
         if (done) {
             return "[X] " + taskName;

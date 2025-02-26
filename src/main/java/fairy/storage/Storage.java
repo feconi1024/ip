b4cd53ff5bf40d16b793b1f78173ed5df5c73cc6
feconi1024 <hzactix@github.com>
2025-02-26 23:57:43 +0800

    Rectify code for statement standard

--- src/main/java/fairy/storage/Storage.java
+++ src/main/java/fairy/storage/Storage.java
@@ -1,6 +1,12 @@
 package fairy.storage;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Iterator;
 
 import fairy.task.Task;
